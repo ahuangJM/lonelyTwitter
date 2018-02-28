ade283485adc9abba2c9d86f51de5ad57d18c706
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-02-27 18:16:29 -0700

    lab7 rev1

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,13 +2,15 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
-/**
- * Created by wz on 14/09/15.
- */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -18,4 +20,33 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         Activity activity = getActivity();
 
     }
+
+    public void setUp() throws Exception {
+        this.solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        this.solo.finishOpenedActivities();
+    }
+
+    public void testAddingTweetToTheList() {
+//        this.solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+//        this.solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet:");
+//        this.solo.clickOnButton("Save");
+//        assertTrue(solo.searchText("Test Tweet"));
+
+//        this.solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+//        assertTrue(solo.searchText("Test Tweet"));
+//        this.solo.clickOnButton("Clear");
+//        assertFalse(this.solo.searchText("Test Tweet"));
+
+
+        this.solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+        this.solo.enterText((EditText) this.solo.getView(R.id.body), "");
+        assertTrue(solo.searchText("Test Tweet"));
+        this.solo.clickOnButton("Clear");
+        assertFalse(this.solo.searchText("Test Tweet"));
+    }
+
 }
