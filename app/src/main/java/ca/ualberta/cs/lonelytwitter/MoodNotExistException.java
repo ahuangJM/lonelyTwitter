85f49067a640b4cc79d81e473906d72fb3ec0615
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-02-27 17:09:21 -0700

    lab7 source

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/MoodNotExistException.java
+++
@@ -1,8 +0,0 @@
-package ca.ualberta.cs.lonelytwitter;
-
-/**
- * Created by AlanJ on 2018-01-16.
- */
-
-public class MoodNotExistException extends Exception {
-}
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++
@@ -1,8 +0,0 @@
-package ca.ualberta.cs.lonelytwitter;
-
-/**
- * Created by dezfuli on 1/16/18.
- */
-
-public class TweetTooLongException extends Exception {
-}
